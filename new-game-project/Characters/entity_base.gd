5a920bfd667f775db1019485643de1bf64802123
chrisrudzki <chrisrudzki7@gmail.com>
2024-12-28 20:41:00 -0800

    Delete new-game-project directory

--- new-game-project/Characters/entity_base.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-
-
-var x: int = 10
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
-@onready var sprite = $Sprite2D

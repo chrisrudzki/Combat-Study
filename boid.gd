3809107cfea31133815cfc58b67c370e7e45b9e6
chrisrudzki <chrisrudzki7@gmail.com>
2024-12-29 13:06:25 -0800

    Update boid.gd

--- boid.gd
+++ boid.gd
@@ -7,12 +7,21 @@ var neighbours = []
 var neighbours_and_p = []
 var boid_num
 var player
+var player_in_boid
+var boid_avoid
 @onready var raycast1 = $RayCast2D
 @onready var raycast2 = $RayCast2D2
 @onready var raycast3 = $RayCast2D3
 @onready var raycast4 = $RayCast2D4
 
 @onready var timer = $Timer
+@onready var timer2 = $Timer2
+
+var melee_dmg = 35
+
+var melee_cooldown = false
+var is_attacking = false
+
 
 func all_raycasts():
 	
@@ -24,10 +33,8 @@ func all_neighbours():
 	return neighbours
 	
 	
-func all_neighbours_and_p():
-	
-	
-	return neighbours_and_p
+func does_boid_avoid():
+	return boid_avoid
 
 func slow_boid():
 	movement_speed = .5
@@ -40,31 +47,31 @@ func boid():
 func get_player():
 	if player != null:
 		return player
+		
+
 	
 func _physics_process(delta: float):
 	
 	
 	var parent = get_parent()
+	var player = parent.get_node("Player")
 	
-	if parent.has_node("Player"):
-		
-		#parent.Player.global_position
-		var player = parent.get_node("Player")
-		
-		#print(player.global_position)
-		
-		var dir_to_player = self.global_position.direction_to(player.global_position)
-		
-		dir_to_player = dir_to_player.normalized()
-		
-		
-		#print("rotation angle ", dir_to_player.angle())
-		rotation = lerp_angle(rotation, dir_to_player.angle(), .4)
-		
-		
-		#print("dir_to_player,  movement_speed ", dir_to_player, " ", movement_speed )
-		velocity = dir_to_player * movement_speed
+	if is_attacking and melee_cooldown == false: #if player is in boid 
+	###!!!
+		player.get_hit(melee_dmg)
+		melee_cooldown = true
+		timer2.wait_time = 2
+		timer2.start()
 	
+
+		
+	var dir_to_player = self.global_position.direction_to(player.global_position)
+		
+	dir_to_player = dir_to_player.normalized()
+		
+	rotation = lerp_angle(rotation, dir_to_player.angle(), .4)
+		
+	velocity = dir_to_player * movement_speed
 	
 	
 	move_and_collide(velocity)
@@ -72,14 +79,19 @@ func _physics_process(delta: float):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	if body.has_method("player"):
-		if body.boid_avoids_p == true:
-			player = body
-			neighbours_and_p.append(body)
-			#neighbours.append(body)
-	
+		player_in_boid = true
+		
+		if body.boid_avoids == true:#player is rolling
+			boid_avoid = true
+			
+		else:
+			is_attacking = true
+			#player = body
+			
+			
 	if (body.has_method("boid") and body.boid_num != boid_num):
 		neighbours.append(body)
-		neighbours_and_p.append(body)#!
+		
 	
 	
 func _on_area_2d_body_exited(body: Node2D) -> void:
@@ -88,8 +100,14 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		neighbours.erase(neighbours.find(body))
 	
 	if body.has_method("player"):
-		player = null
+		player_in_boid = false
+		is_attacking = false
+
 
 
 func _on_timer_timeout() -> void:
 	movement_speed = 3.5
+
+
+func _on_timer_2_timeout() -> void:
+	melee_cooldown = false
